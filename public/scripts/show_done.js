5a36d0cfd11087cda883e529ab8e0e5e54bc7d16
Aliksalot <117526291+Aliksalot@users.noreply.github.com>
2023-06-19 19:39:09 +0300

    Delete show_done.js

--- public/scripts/show_done.js
+++
@@ -1,9 +0,0 @@
-
-const show_done = () => {
-    const buttonBox = document.getElementById('taskDisplayBox')
-    const showDoneButton = document.getElementById('showDoneButton')
-    buttonBox.className = buttonBox.className === "NODONE" ? "ALL" : "NODONE"
-    showDoneButton.textContent = buttonBox.className === "NODONE" ? "Show all tasks" : 'Hide finished tasks'
-    write_all_tasks()
-    console.log(buttonBox.className)
-}
